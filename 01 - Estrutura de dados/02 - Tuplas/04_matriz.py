9911a61a40bb9257d663066b1ab028c87d09a11e
Rafael Pinheiro <82719410+radarkness@users.noreply.github.com>
2024-06-04 14:34:15 -0300

    04/06/2024

--- 01 - Estrutura de dados/02 - Tuplas/04_matriz.py
+++ 01 - Estrutura de dados/02 - Tuplas/04_matriz.py
@@ -6,5 +6,5 @@ matriz = (
 
 print(matriz[0])  # (1, "a", 2)
 print(matriz[0][0])  # 1
-print(matriz[0][-1])  # 2
+print(matriz[1][-1])  # 4
 print(matriz[-1][-1])  # "c"
--- 01 - Estrutura de dados/03 - Conjuntos/00_declarando_conjuntos.py
+++ 01 - Estrutura de dados/03 - Conjuntos/00_declarando_conjuntos.py
@@ -6,3 +6,8 @@ print(letras)  # {"b", "a", "c", "x", "i"}
 
 carros = set(("palio", "gol", "celta", "palio"))
 print(carros)  # {"gol", "celta", "palio"}
+
+bandas = {"Evile", "System of a Down", "Offspring", "Evile", "Symphony X", "Offspring", "Evile"} 
+bandas = list(bandas)
+print(len(bandas)) 
+print(bandas[0])
